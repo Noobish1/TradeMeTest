278ea80f1eed6e9624d320c102fe74dd1e5eb08e
Blair McArthur <blair.mcarthur@skipthedishes.com>
2017-08-16 12:24:17 +1200

    setup the basic project

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'cocoapods', '1.1.1'
+gem 'xcodeproj', '1.4.1'
+gem 'synx', '0.2.1'
---
+++ TradeMeTest/Application/AppDelegate.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+@UIApplicationMain
+internal final class AppDelegate: UIResponder, UIApplicationDelegate {
+    internal var window: UIWindow?
+    
+    internal func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
