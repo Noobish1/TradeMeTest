426034c52bbdaf4bee1ac2e43a7103560dfaf102
Blair McArthur <blair.mcarthur@skipthedishes.com>
2017-08-20 17:30:16 +1200

    cleaned up the viewmodels

--- TradeMeTest/ViewModels/CategoryViewModel.swift
+++ TradeMeTest/ViewModels/CategoryViewModel.swift
@@ -1,15 +1,17 @@
 import UIKit
 
 internal struct CategoryViewModel {
+    // MARK: properties
     internal let name: String
     internal let number: String
     internal let hasSubcategories: Bool
     internal let subcategoires: [CategoryViewModel]
-    
+    // MARK: computed properties
     internal var accessoryType: UITableViewCellAccessoryType {
         return hasSubcategories ? .disclosureIndicator : .none
     }
     
+    // MARK: init/deinit
     internal init(name: String? = nil, category: Category) {
         // It seems like you do this in the Trade Me app unless the names are different in the production endpoint
         // I decided to copy what the existing app does
--- TradeMeTest/ViewModels/ListingViewModel.swift
+++ TradeMeTest/ViewModels/ListingViewModel.swift
@@ -1,9 +1,11 @@
 import Foundation
 
 internal struct ListingViewModel {
+    // MARK: properties
     internal let title: String
     internal let imageURL: URL?
     
+    // MARK: init/deinit
     internal init(listing: Listing) {
         self.title = listing.title
         self.imageURL = listing.imageURLString.flatMap(URL.init)
