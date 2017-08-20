06c27292a2fc8b55f8e795b0c4490f30f97bec7b
Blair McArthur <blair.mcarthur@skipthedishes.com>
2017-08-20 16:41:02 +1200

    cleaned up the AppDelegate

--- TradeMeTest/Application/AppDelegate.swift
+++ TradeMeTest/Application/AppDelegate.swift
@@ -1,10 +1,14 @@
 import UIKit
 import Then
 
+// MARK: AppDelegate
 @UIApplicationMain
-internal final class AppDelegate: UIResponder, UIApplicationDelegate {
+internal final class AppDelegate: UIResponder {
     internal var window: UIWindow?
-    
+}
+
+// MARK: UIApplicationDelegate
+extension AppDelegate: UIApplicationDelegate {
     internal func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         self.window = UIWindow(frame: UIScreen.main.bounds).then {
             $0.rootViewController = RootViewController()
